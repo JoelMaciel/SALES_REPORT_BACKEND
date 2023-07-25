fb72fe6d94da72818fbf395ab86a71ff4c94c7ec
Joel Maciel <soldado25461@gmail.com>
2023-07-25 16:06:24 -0300

    fix: formatting the DTO date

--- src/main/java/com/joelmaciel/salesreport/api/dto/response/ProductDTO.java
+++ src/main/java/com/joelmaciel/salesreport/api/dto/response/ProductDTO.java
@@ -1,28 +1,35 @@
 package com.joelmaciel.salesreport.api.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.joelmaciel.salesreport.domain.model.Product;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 @Builder
 @Getter
 @Setter
 public class ProductDTO {
 
+    private Long id;
     private String name;
     private String description;
     private BigDecimal price;
     private String sku;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private OffsetDateTime creationDate;
 
     public static ProductDTO toDTO(Product product) {
         return ProductDTO.builder()
+                .id(product.getId())
                 .name(product.getName())
                 .description(product.getDescription())
                 .price(product.getPrice())
                 .sku(product.getSku())
+                .creationDate(product.getCreationDate())
                 .build();
     }
 }
